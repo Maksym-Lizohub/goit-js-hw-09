c2d63e9ad4c126b4eb62b1b761deb26c416bcfaf
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-07-04 21:53:43 +0300

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const refs = {
+  onBtnStartChangeColor: document.querySelector('[data-start]'),
+  onBodyColor: document.querySelector('body'),
+  onBtnStop: document.querySelector('[data-stop]'),
+};
+
+const onChangeColor = () => {
+  timerId = setInterval(() => {
+    let bckgndColor = getRandomHexColor();
+    refs.onBodyColor.style.backgroundColor = bckgndColor;
+    console.dir(bckgndColor);
+  }, 1000);
+};
+
+refs.onBtnStartChangeColor.addEventListener('click', onChangeColor);
+
+refs.onBtnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+});
